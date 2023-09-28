a91f55de0f09988723e9961d8c3be652c933326a
unknown <axel.ein@gmail.com>
2023-09-29 01:18:35 +0200

    ===E===[TEMPLATE REFACTOR] Controller, View : refactored category templates with multiple sections

--- src/Controller/PersonnageController.php
+++ src/Controller/PersonnageController.php
@@ -17,9 +17,28 @@ class PersonnageController extends AbstractController
         $pjs = $personnageRepository->findAllPJsSorted();
         $pnjs = $personnageRepository->findAllPNJsSorted();
 
+        $sections = [];
+        $sections[0]['name'] = "PJs";
+        $sections[0]['titleLight'] = 'Personnages';
+        $sections[0]['titleStrong'] = 'Joueurs';
+        $sections[0]['element'] = 'personnage';
+
+        $sections[1]['name'] = "PNJs";
+        $sections[1]['titleLight'] = 'Personnages';
+        $sections[1]['titleStrong'] = 'Non-Joueurs';
+        $sections[1]['element'] = 'personnage';
+
+        $header_classname = 'characters';
+        $header_up = "Les Héros de l'Aventure";
+        $header_down = 'Les Personnages';
+
         return $this->render('personnage/index.html.twig', [
             'pjs' => $pjs,
             'pnjs' => $pnjs,
+            'sections' => $sections,
+            'header_classname' => $header_classname,
+            'header_up' => $header_up,
+            'header_down' => $header_down,
         ]);
     }
 

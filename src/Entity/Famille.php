6d537554a2cac7c47ff357e4c8c7e638fad0aa30
unknown <axel.ein@gmail.com>
2023-10-08 14:55:51 +0200

    [FAMILLE] added entity, repo, formtype

---
+++ src/Entity/Famille.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FamilleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=FamilleRepository::class)
+ */
+class Famille
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Clan::class, inversedBy="familles")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $clan;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $mon;
+
+    /**
+     * @ORM\OneToOne(targetEntity=Personnage::class, cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $chef;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getClan(): ?Clan
+    {
+        return $this->clan;
+    }
+
+    public function setClan(?Clan $clan): self
+    {
+        $this->clan = $clan;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getMon(): ?string
+    {
+        return $this->mon;
+    }
+
+    public function setMon(?string $mon): self
+    {
+        $this->mon = $mon;
+
+        return $this;
+    }
+
+    public function getChef(): ?Personnage
+    {
+        return $this->chef;
+    }
+
+    public function setChef(Personnage $chef): self
+    {
+        $this->chef = $chef;
+
+        return $this;
+    }
+}

ede4f06b8f9fa647caf5681452281b38a5d69edb
Axel <axel.ein@gmail.com>
2023-10-09 23:35:07 +0200

    [CLAN] updated wuth new prop. Long Description and Territory Map

---
+++ migrations/Version20231009163436.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231009163436 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE clan ADD territoire_carte VARCHAR(255) DEFAULT NULL, ADD territoire_desc LONGTEXT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE clan DROP territoire_carte, DROP territoire_desc');
+    }
+}

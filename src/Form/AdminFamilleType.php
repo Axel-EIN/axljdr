6cf1cb3e5e53085a9205c839c59db0eaf53282ab
Axel <axel.ein@gmail.com>
2023-10-18 18:57:35 +0200

    =E=[CLAN formType, SCHOOL formType, FAMILY formType] added constraints

--- src/Form/AdminFamilleType.php
+++ src/Form/AdminFamilleType.php
@@ -13,15 +13,17 @@ use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
+use Symfony\Component\Validator\Constraints\Length;
+
 class AdminFamilleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom', TextType::class)
+            ->add('nom', TextType::class, array( 'constraints' => [ new Length( [ 'max' => 30 ] ) ] ) )
             ->add('mon', FileType::class, array('mapped' => false, 'data_class' => null, 'required' => false))
-            ->add('description', TextareaType::class)
-            ->add('bonus', TextType::class)
+            ->add('description', TextareaType::class, array( 'required' => false, 'constraints' => [ new Length( [ 'max' => 1000 ] ) ] ))
+            ->add('bonus', TextType::class, array( 'constraints' => [ new Length( [ 'max' => 30 ] ) ] ) )
             ->add('clan', EntityType::class, [
                 'class' => Clan::class,
                 'choice_label' => 'nom',

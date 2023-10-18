04d873aaa9d225eeb3784aae119f32f31e3236ad
Axel <axel.ein@gmail.com>
2023-10-18 18:57:35 +0200

    ===E===[CLAN, ECOLE, FAMILLE] FormType : added constraints

--- src/Form/AdminEcoleType.php
+++ src/Form/AdminEcoleType.php
@@ -13,31 +13,33 @@ use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
+use Symfony\Component\Validator\Constraints\Length;
+
 class AdminEcoleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom', TextType::class)
-            ->add('image', FileType::class, array('mapped' => false, 'data_class' => null, 'required' => false))
-            ->add('bonus', TextType::class)
-            ->add('description', TextareaType::class)
-            ->add('competences', TextareaType::class)
-            ->add('equipements', TextareaType::class)
-            ->add('tech1Nom', TextType::class, ['required' => false])
-            ->add('tech1Desc', TextareaType::class, ['required' => false])
-            ->add('tech2Nom', TextType::class, ['required' => false])
-            ->add('tech2Desc', TextareaType::class, ['required' => false])
-            ->add('tech3Nom', TextType::class, ['required' => false])
-            ->add('tech3Desc', TextareaType::class, ['required' => false])
-            ->add('tech4Nom', TextType::class, ['required' => false])
-            ->add('tech4Desc', TextareaType::class, ['required' => false])
-            ->add('tech5Nom', TextType::class, ['required' => false])
+            ->add('nom', TextType::class, [ 'constraints' => [ new Length( [ 'max' => 60 ] ) ] ] )
+            ->add('image', FileType::class, [ 'required' => false, 'mapped' => false, 'data_class' => null ])
+            ->add('bonus', TextType::class, [ 'required' => false , 'constraints' => [ new Length( [ 'max' => 20 ] ) ] ])
+            ->add('description', TextareaType::class, [ 'required' => false, 'constraints' => [ new Length( [ 'max' => 600 ] ) ] ])
+            ->add('competences', TextareaType::class, [ 'required' => false, 'constraints' => [ new Length( [ 'max' => 600 ] ) ] ])
+            ->add('equipements', TextareaType::class, [ 'required' => false, 'constraints' => [ new Length( [ 'max' => 600 ] ) ] ])
+            ->add('tech1Nom', TextType::class, ['required' => false, 'constraints' => [ new Length( [ 'max' => 100 ] ) ] ])
+            ->add('tech1Desc', TextareaType::class, ['required' => false] )
+            ->add('tech2Nom', TextType::class, ['required' => false, 'constraints' => [ new Length( [ 'max' => 100 ] ) ]])
+            ->add('tech2Desc', TextareaType::class, ['required' => false] )
+            ->add('tech3Nom', TextType::class, ['required' => false, 'constraints' => [ new Length( [ 'max' => 100 ] ) ] ])
+            ->add('tech3Desc', TextareaType::class, ['required' => false ] )
+            ->add('tech4Nom', TextType::class, ['required' => false, 'constraints' => [ new Length( [ 'max' => 100 ] ) ] ])
+            ->add('tech4Desc', TextareaType::class, ['required' => false ] )
+            ->add('tech5Nom', TextType::class, ['required' => false, 'constraints' => [ new Length( [ 'max' => 100 ] ) ] ])
             ->add('tech5Desc', TextareaType::class, ['required' => false])
-            ->add('techSpecialNom', TextType::class, ['required' => false])
+            ->add('techSpecialNom', TextType::class, ['required' => false, 'constraints' => [ new Length( [ 'max' => 100 ] ) ] ])
             ->add('techSpecialDesc', TextareaType::class, ['required' => false])
-            ->add('affinite', TextType::class, ['required' => false])
-            ->add('deficience', TextType::class, ['required' => false])
+            ->add('affinite', TextType::class, ['required' => false, 'constraints' => [ new Length( [ 'max' => 50 ] ) ] ])
+            ->add('deficience', TextType::class, ['required' => false, 'constraints' => [ new Length( [ 'max' => 50 ] ) ] ])
             ->add('sorts', TextareaType::class, ['required' => false])
             ->add('classe', EntityType::class, [
                 'class' => Classe::class,

5e7b37aed2028b16b0837d3db1fd23fd3282a157
Axel <axel.ein@gmail.com>
2023-10-27 04:37:06 +0200

    [RULE] added FEATURE

---
+++ src/Form/AdminRuleType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Rule;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Validator\Constraints\Length;
+
+class AdminRuleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [ 'constraints' => [ new Length( [ 'max' => 100 ] ) ] ] )
+            ->add('base', CheckboxType::class, ['required' => false])
+            ->add('image', FileType::class, array('mapped' => false, 'data_class' => null, 'required' => false))
+            ->add('pdf', FileType::class, ['required' => false])
+            ->add('part1titre', TextType::class, ['required' => false, 'constraints' => [ new Length( [ 'max' => 100 ] ) ] ] )
+            ->add('part1', TextareaType::class, ['required' => false])
+            ->add('part1aside', textareaType::class, ['required' => false])
+            ->add('part2titre', TextType::class, ['required' => false, 'constraints' => [ new Length( [ 'max' => 100 ] ) ] ] )
+            ->add('part2', TextareaType::class, ['required' => false])
+            ->add('part2aside', textareaType::class, ['required' => false])
+            ->add('part3titre', TextType::class, ['required' => false, 'constraints' => [ new Length( [ 'max' => 100 ] ) ] ] )
+            ->add('part3', TextareaType::class, ['required' => false])
+            ->add('part3aside', textareaType::class, ['required' => false])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Rule::class,
+        ]);
+    }
+}

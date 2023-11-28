44a3ce46d8237e3c0078286f910ecdb1e22c0406
Axel <axel.ein@gmail.com>
2023-11-28 03:56:52 +0100

    ==E==[CSS library, SCRIPT library, LIBRARY form, detail] updated

--- public/scripts/library.js
+++ public/scripts/library.js
@@ -2,7 +2,8 @@ $(document).ready(function ()
 { // quand la page est prête
     $("#filter-select").change(function() { // sur l'objet DOM dont l'ID est filter-select, on ajoute un écouteur on change dont la fonction call back fait :
         var optionValue = $(this).val(); // crée une variable optionValue et stock la valeur selectionné dans le dropdown
-        var url = window.location.href.split("&")[0]; // prend l'url window.location.hret puis applique une division sur le caractère ?, puis prend la premiere partie de l'array
+        var url = window.location.href.split("&filter=")[0]; // prend l'url window.location.href puis applique une division sur le caractère ?, puis prend la premiere partie de l'array
+        // var url = window.location.href; // prend l'url window.location.hret puis applique une division sur le caractère ?, puis prend la premiere partie de l'array
         window.location = url + "&filter=" + optionValue; // s'il n'y a pas de ? on ajoute le caractère ? en plus de l'option
     });
 

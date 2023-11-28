c8c90da638b0e6863745e23bd38395b577ead7ad
Axel <axel.ein@gmail.com>
2023-11-28 04:19:48 +0100

    [LIBRARY controller] added in backoffice

--- src/Controller/BackOfficeController.php
+++ src/Controller/BackOfficeController.php
@@ -2,161 +2,172 @@
 
 namespace App\Controller;
 
-use App\Repository\SaisonRepository;
-use App\Repository\ChapitreRepository;
-use App\Repository\EpisodeRepository;
-use App\Repository\SceneRepository;
-use App\Repository\PersonnageRepository;
-use App\Repository\FichePersonnageRepository;
 use App\Repository\ClanRepository;
-use App\Repository\FamilleRepository;
-use App\Repository\ArchiveRepository;
 use App\Repository\LieuRepository;
 use App\Repository\RuleRepository;
-use App\Repository\ClasseRepository;
 use App\Repository\EcoleRepository;
-use App\Repository\AvantageRepository;
-use App\Repository\CompetenceRepository;
 use App\Repository\ObjetRepository;
+use App\Repository\SceneRepository;
+use App\Repository\ClasseRepository;
+use App\Repository\SaisonRepository;
+use App\Repository\ArchiveRepository;
+use App\Repository\EpisodeRepository;
+use App\Repository\FamilleRepository;
+use App\Repository\LibraryRepository;
+use App\Repository\AvantageRepository;
+use App\Repository\ChapitreRepository;
+use App\Repository\CompetenceRepository;
+use App\Repository\PersonnageRepository;
 use App\Repository\UtilisateurRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\FichePersonnageRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class BackOfficeController extends AbstractController
 {
     /**
      * @Route("/back-office", name="back_office")
      */
-    public function viewBackOffice(SaisonRepository $saisonRepository,
-                                       ChapitreRepository $chapitreRepository,
-                                       EpisodeRepository $episodeRepository,
-                                       SceneRepository $sceneRepository,
+    public function viewBackOffice(
+        SaisonRepository $saisonRepository,
+        ChapitreRepository $chapitreRepository,
+        EpisodeRepository $episodeRepository,
+        SceneRepository $sceneRepository,
 
-                                       ClanRepository $clanRepository,
-                                       FamilleRepository $familleRepository,
-                                       ArchiveRepository $archiveRepository,
-                                       LieuRepository $lieuRepository,
+        ClanRepository $clanRepository,
+        FamilleRepository $familleRepository,
+        ArchiveRepository $archiveRepository,
+        LieuRepository $lieuRepository,
 
-                                       RuleRepository $ruleRepository,
-                                       ClasseRepository $classeRepository,
-                                       EcoleRepository $ecoleRepository,
-                                       AvantageRepository $avantageRepository,
-                                       CompetenceRepository $competenceRepository,
-                                       ObjetRepository $objetRepository,
+        RuleRepository $ruleRepository,
+        LibraryRepository $libraryRepository,
+        ClasseRepository $classeRepository,
+        EcoleRepository $ecoleRepository,
+        AvantageRepository $avantageRepository,
+        CompetenceRepository $competenceRepository,
+        ObjetRepository $objetRepository,
 
-                                       PersonnageRepository $personnageRepository,
-                                       FichePersonnageRepository $fichePersonnageRepository,
+        PersonnageRepository $personnageRepository,
+        FichePersonnageRepository $fichePersonnageRepository,
 
-                                       UtilisateurRepository $utilisateurRepository
-                                       ): Response
+        UtilisateurRepository $utilisateurRepository ): Response
     {
 
-        if( !$this->isGranted('ROLE_ADMIN') && !$this->isGranted('ROLE_MJ') ) // Si pas Admin ou pas MJ alors redirection
+        if(
+            !$this->isGranted('ROLE_ADMIN')
+         || !$this->isGranted('ROLE_MJ')
+         )
             throw new \Exception('Permission denied!');
 
         // AVENTURE
         $nbrSaisons = $saisonRepository->countSaisons();
-        $dernierSaison = $saisonRepository->findOneBy(array(),array('id' => 'DESC'));
+        $lastSaison = $saisonRepository->findOneBy(array(),array('id' => 'DESC'));
 
         $nbrChapitres = $chapitreRepository->countChapitres();
-        $dernierChapitre = $chapitreRepository->findOneBy(array(),array('id' => 'DESC'));
+        $lastChapitre = $chapitreRepository->findOneBy(array(),array('id' => 'DESC'));
 
         $nbrEpisodes = $episodeRepository->countEpisodes();
-        $dernierEpisode = $episodeRepository->findOneBy(array(),array('id' => 'DESC'));
+        $lastEpisode = $episodeRepository->findOneBy(array(),array('id' => 'DESC'));
 
         $nbrScenes = $sceneRepository->countScenes();
-        $dernierScene = $sceneRepository->findOneBy(array(),array('id' => 'DESC'));
+        $lastScene = $sceneRepository->findOneBy(array(),array('id' => 'DESC'));
 
         // CHARACTERS
         $nbrPJs = $personnageRepository->countPJs();
-        $dernierPJ = $personnageRepository->findOneBy(array("estPj" => "1"),array('id' => 'DESC'));
+        $lastPJ = $personnageRepository->findOneBy(array("estPj" => "1"),array('id' => 'DESC'));
 
         $nbrPNJs = $personnageRepository->countPNJs();
-        $dernierPNJ = $personnageRepository->findOneBy(array("estPj" => "0"),array('id' => 'DESC'));
+        $lastPNJ = $personnageRepository->findOneBy(array("estPj" => "0"),array('id' => 'DESC'));
 
         $nbrFiches = $fichePersonnageRepository->countFiches();
-        $derniereFiche = $fichePersonnageRepository->findOneBy(array(),array('id' => 'DESC'));
+        $lastFiche = $fichePersonnageRepository->findOneBy(array(),array('id' => 'DESC'));
 
         // EMPIRE
         $nbrClans = $clanRepository->countClans();
-        $dernierClan = $clanRepository->findOneBy(array(),array('id' => 'DESC'));
+        $lastClan = $clanRepository->findOneBy(array(),array('id' => 'DESC'));
 
         $nbrFamilles = $familleRepository->countFamilles();
-        $derniereFamille = $familleRepository->findOneBy(array(),array('id' => 'DESC'));
+        $lastFamille = $familleRepository->findOneBy(array(),array('id' => 'DESC'));
         
         $nbrArchives = $archiveRepository->countArchives();
-        $derniereArchive = $archiveRepository->findOneBy(array(),array('id' => 'DESC'));
+        $lastArchive = $archiveRepository->findOneBy(array(),array('id' => 'DESC'));
 
         $nbrLieux = $lieuRepository->countLieux();
-        $dernierLieu = $lieuRepository->findOneBy(array(),array('id' => 'DESC'));
+        $lastLieu = $lieuRepository->findOneBy(array(),array('id' => 'DESC'));
 
         // RULES
         $nbrRules = $ruleRepository->countRules();
-        $derniereRule = $ruleRepository->findOneBy(array(),array('id' => 'DESC'));
+        $lastRule = $ruleRepository->findOneBy(array(),array('id' => 'DESC'));
+
+        $nbrLibraries = $libraryRepository->countLibraries();
+        $lastLibrary = $libraryRepository->findOneBy(array(),array('id' => 'DESC'));
 
         $nbrClasses = $classeRepository->countClasses();
-        $dernierClasse = $classeRepository->findOneBy(array(),array('id' => 'DESC'));
+        $lastClasse = $classeRepository->findOneBy(array(),array('id' => 'DESC'));
 
         $nbrEcoles = $ecoleRepository->countEcoles();
-        $dernierEcole = $ecoleRepository->findOneBy(array(),array('id' => 'DESC'));
+        $lastEcole = $ecoleRepository->findOneBy(array(),array('id' => 'DESC'));
 
         $nbrAvantages = $avantageRepository->countAvantages();
-        $dernierAvantage = $avantageRepository->findOneBy(array(),array('id' => 'DESC'));
+        $lastAvantage = $avantageRepository->findOneBy(array(),array('id' => 'DESC'));
 
         $nbrCompetences = $competenceRepository->countCompetences();
-        $derniereCompetence = $competenceRepository->findOneBy(array(),array('id' => 'DESC'));
+        $lastCompetence = $competenceRepository->findOneBy(array(),array('id' => 'DESC'));
 
         $nbrObjets = $objetRepository->countObjets();
-        $dernierObjet = $objetRepository->findOneBy(array(),array('id' => 'DESC'));
+        $lastObjet = $objetRepository->findOneBy(array(),array('id' => 'DESC'));
 
         // USER
         $nbrUtilisateurs = $utilisateurRepository->countUtilisateurs();
-        $dernierUtilisateur = $utilisateurRepository->findOneBy(array(),array('id' => 'DESC'));
+        $lastUtilisateur = $utilisateurRepository->findOneBy(array(),array('id' => 'DESC'));
 
         return $this->render('back_office/index.html.twig', [
             'controller_name' => 'BackOfficeController',
 
             'nbrSaisons' => $nbrSaisons,
-            'dernierSaison' => $dernierSaison,
+            'lastSaison' => $lastSaison,
             'nbrChapitres' => $nbrChapitres,
-            'dernierChapitre' => $dernierChapitre,
+            'lastChapitre' => $lastChapitre,
             'nbrEpisodes' => $nbrEpisodes,
-            'dernierEpisode' => $dernierEpisode,
+            'lastEpisode' => $lastEpisode,
             'nbrScenes' => $nbrScenes,
-            'dernierScene' => $dernierScene,
+            'lastScene' => $lastScene,
 
             'nbrPJs' => $nbrPJs,
-            'dernierPJ' => $dernierPJ,
+            'lastPJ' => $lastPJ,
             'nbrPNJs' => $nbrPNJs,
-            'dernierPNJ' => $dernierPNJ,
+            'lastPNJ' => $lastPNJ,
             'nbrFiches' => $nbrFiches,
-            'derniereFiche' => $derniereFiche,
+            'lastFiche' => $lastFiche,
 
             'nbrClans' => $nbrClans,
-            'dernierClan' => $dernierClan,
+            'lastClan' => $lastClan,
             'nbrFamilles' => $nbrFamilles,
-            'derniereFamille' => $derniereFamille,
+            'lastFamille' => $lastFamille,
             'nbrArchives' => $nbrArchives,
-            'derniereArchive' => $derniereArchive,
+            'lastArchive' => $lastArchive,
             'nbrLieux' => $nbrLieux,
-            'dernierLieu' => $dernierLieu,
+            'lastLieu' => $lastLieu,
 
             'nbrRules' => $nbrRules,
-            'derniereRule' => $derniereRule,
+            'lastRule' => $lastRule,
+            'nbrLibraries' => $nbrLibraries,
+            'lastLibrary' => $lastLibrary,
             'nbrClasses' => $nbrClasses,
-            'dernierClasse' => $dernierClasse,
+            'lastClasse' => $lastClasse,
             'nbrEcoles' => $nbrEcoles,
-            'dernierEcole' => $dernierEcole,
+            'lastEcole' => $lastEcole,
             'nbrAvantages' => $nbrAvantages,
-            'dernierAvantage' => $dernierAvantage,
+            'lastAvantage' => $lastAvantage,
             'nbrCompetences' => $nbrCompetences,
-            'derniereCompetence' => $derniereCompetence,
+            'lastCompetence' => $lastCompetence,
             'nbrObjets' => $nbrObjets,
-            'dernierObjet' => $dernierObjet,
+            'lastObjet' => $lastObjet,
 
             'nbrUtilisateurs' => $nbrUtilisateurs,
-            'dernierUtilisateur' => $dernierUtilisateur,
+            'lastUtilisateur' => $lastUtilisateur,
         ]);
     }
 }

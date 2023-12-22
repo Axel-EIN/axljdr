3be35dd46b0dbb6db8c8ebffa4c28690fd708a08
Axel <axel.ein@gmail.com>
2023-12-22 05:22:43 +0100

    [SORT controller] updated Controllers for numero, original name and keywords

--- src/Controller/AdminSortController.php
+++ src/Controller/AdminSortController.php
@@ -35,6 +35,7 @@ class AdminSortController extends AbstractController
             'extra_col1' => 'categorie',
             'extra_col2' => 'anneau',
             'extra_col3' => 'niveau',
+            'extra_col4' => 'numero',
         ]);
     }
 
@@ -46,10 +47,32 @@ class AdminSortController extends AbstractController
 
         $sort = new Sort;
 
+        // URL PARAM PRE-FILL
+        $param_numero = $request->query->get('numero');
+        if ( !empty($param_numero) && is_numeric($param_numero) && $param_numero > 0 )
+            $sort->setNumero($param_numero);
+
+        $param_categorie = $request->query->get('tab');
+        if ( !empty($param_categorie) && !is_numeric($param_categorie) )
+            $sort->setCategorie($param_categorie);
+
+        $param_anneau = $request->query->get('subtab');
+        if ( !empty($param_anneau) && !is_numeric($param_anneau) )
+            $sort->setAnneau($param_anneau);
+
         $form = $this->createForm(AdminSortType::class, $sort);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            // GET NUMERO
+            $formNumero = $form->get('numero')->getData();
+            $fratrieArrivee = $sortRepository->findBy(['anneau' => $form->get('anneau')->getData()]);
+
+            if ( empty($formNumero) || !is_numeric($formNumero) || $formNumero < 1 )
+                $sort->setNumero( count($fratrieArrivee) + 1 );
+            else
+                $sort->setNumero( $formNumero );
 
             $em->persist($sort);
             $em->flush();
@@ -58,7 +81,7 @@ class AdminSortController extends AbstractController
             // REDIRECTION
             if (!empty($request->query->get('redirect')) && $request->query->get('redirect') == 'library'
             && !empty($request->query->get('libraryID')) && $request->query->get('libraryID') > 0)
-                return $this->redirectToRoute( 'regles_library', [ 'id' => $request->query->get('libraryID') , 'tab' => $sort->getCategorie() ] );
+                return $this->redirectToRoute( 'regles_library', [ 'id' => $request->query->get('libraryID') , 'tab' => $sort->getCategorie() , 'subtab' => $sort->getAnneau() ] );
 
             return $this->redirectToRoute('admin_sort');
         }
@@ -78,20 +101,36 @@ class AdminSortController extends AbstractController
      * @Route("/admin/sort/{id}/edit", name="admin_sort_edit")
      * @IsGranted("ROLE_MJ")
      */
-    public function editSort(Request $request, Sort $sort, FileHandler $fileHandler, SortRepository $sortRepository, Numeroteur $numeroteur): Response {
+    public function editSort(Request $request, Sort $sort, SortRepository $sortRepository, Numeroteur $numeroteur): Response {
+
+        // Saving numbler and ID for re-ordering later
+        if ( empty( $sort->getNumero() ) || !is_numeric( $sort->getNumero() ) || $sort->getNumero() < 0)
+            $numeroDepart = -1;
+        else
+            $numeroDepart = $sort->getNumero();
+
+        $fratrieDepartId = $sort->getAnneau();
 
         $form = $this->createForm(AdminSortType::class, $sort);
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()) {
 
+            // RE-ORDERING : if number has changed or if parent has changed
+            if ($numeroDepart != $sort->getNumero() || $fratrieDepartId != $sort->getAnneau())
+            {
+                $fratrieDepart = $sortRepository->findBy(['anneau' => $fratrieDepartId]);
+                $fratrieArrivee = $sortRepository->findBy(['anneau' => $sort->getAnneau()]);
+                $numeroteur->reordonnerNumero($sort->getId(), $numeroDepart, $sort->getNumero(), $fratrieDepart, $fratrieArrivee);
+            }
+
             $this->getDoctrine()->getManager()->flush();
-            $this->addFlash('success', "L'sort a bien été modifié.");
+            $this->addFlash('success', "Le Sort a bien été modifié.");
 
             // REDIRECTION
             if (!empty($request->query->get('redirect')) && $request->query->get('redirect') == 'library'
             && !empty($request->query->get('libraryID')) && $request->query->get('libraryID') > 0)
-                return $this->redirectToRoute( 'regles_library', [ 'id' => $request->query->get('libraryID') , 'tab' => $sort->getCategorie() ] );
+                return $this->redirectToRoute( 'regles_library', [ 'id' => $request->query->get('libraryID') , 'tab' => $sort->getCategorie() , 'subtab' => $sort->getAnneau() ] );
 
             return $this->redirectToRoute('admin_sort');
         }
@@ -119,7 +158,7 @@ class AdminSortController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($sort);
             $entityManager->flush();
-            $this->addFlash('success', "L'sort a bien été supprimé.");
+            $this->addFlash('success', "Le Sort a bien été supprimé.");
         }
 
         return $this->redirectToRoute('admin_sort');

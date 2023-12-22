44421807ab0ffb2a33a0bb0065e04bb26db0c8dd
Axel <axel.ein@gmail.com>
2023-12-22 05:12:59 +0100

    [LIBRARY] updated Library page JS for filters

--- public/scripts/library.js
+++ public/scripts/library.js
@@ -1,10 +1,17 @@
-$(document).ready(function ()
-{ // quand la page est prête
+$(document).ready(function () // quand la page est prête
+{   
     $("#filter-select").change(function() { // sur l'objet DOM dont l'ID est filter-select, on ajoute un écouteur on change dont la fonction call back fait :
         var optionValue = $(this).val(); // crée une variable optionValue et stock la valeur selectionné dans le dropdown
         var url = window.location.href.split("&filter=")[0]; // prend l'url window.location.href puis applique une division sur le caractère ?, puis prend la premiere partie de l'array
         // var url = window.location.href; // prend l'url window.location.hret puis applique une division sur le caractère ?, puis prend la premiere partie de l'array
         window.location = url + "&filter=" + optionValue; // s'il n'y a pas de ? on ajoute le caractère ? en plus de l'option
+    });
+
+    $("#keyword-select").change(function() { // sur l'objet DOM dont l'ID est keyword-select, on ajoute un écouteur on change dont la fonction call back fait :
+        var optionValue = $(this).val(); // crée une variable optionValue et stock la valeur selectionné dans le dropdown
+        var url = window.location.href.split("&keyword=")[0]; // prend l'url window.location.href puis applique une division sur le caractère ?, puis prend la premiere partie de l'array
+        // var url = window.location.href; // prend l'url window.location.hret puis applique une division sur le caractère ?, puis prend la premiere partie de l'array
+        window.location = url + "&keyword=" + optionValue; // s'il n'y a pas de ? on ajoute le caractère ? en plus de l'option
     });
 
 });

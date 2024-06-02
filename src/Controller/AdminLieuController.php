b19509da58d9fae38ef381abb58455b7b6818151
Axel <axel.ein@gmail.com>
2024-06-02 19:13:03 +0200

    [LIEU controller] implemented filehandler service

--- src/Controller/AdminLieuController.php
+++ src/Controller/AdminLieuController.php
@@ -3,11 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Lieu;
-use App\Service\Uploader;
+use App\Service\FileHandler;
 use App\Form\AdminLieuType;
 use App\Repository\LieuRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -30,7 +29,7 @@ class AdminLieuController extends AbstractController
      * @Route("/admin/lieu/create", name="admin_lieu_create")
      * @IsGranted("ROLE_MJ")
      */
-    public function ajouterLieu(Request $request, EntityManagerInterface $em, Uploader $uploadeur) {
+    public function ajouterLieu(Request $request, EntityManagerInterface $em, FileHandler $fileHandler) {
 
         $lieu = new Lieu;
         $form = $this->createForm(AdminLieuType::class, $lieu);
@@ -38,32 +37,29 @@ class AdminLieuController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
 
-            $nouvelleImage = $form->get('image')->getData();
-            $nouvelleCarte = $form->get('carte')->getData();
-            $nouvelleRegion = $form->get('region')->getData();
 
+            $nouvelleImage = $form->get('image')->getData();
             if (!empty($nouvelleImage)) {
-                $nouvelleImageNomFichier = $uploadeur->upload($nouvelleImage, 'lieu-' . $lieu->getNom() . '-image', 'lieux');
-                $nouveauCheminRelatif = 'assets/img/lieux/' . $nouvelleImageNomFichier;
-                $lieu->setImage($nouveauCheminRelatif);
+                $prefix = 'lieu-' . $lieu->getNom() . '-image';
+                $lieu->setImage($fileHandler->handle($nouvelleImage, null, $prefix, 'lieux'));
             } else { $lieu->setImage('assets/img/placeholders/1280x720.png'); }
 
+            $nouvelleCarte = $form->get('carte')->getData();
             if (!empty($nouvelleCarte)) {
-                $nouvelleCarteNomFichier = $uploadeur->upload($nouvelleCarte, 'lieu-' . $lieu->getNom() . '-carte', 'lieux');
-                $nouveauCheminRelatif = 'assets/img/lieux/' . $nouvelleCarteNomFichier;
-                $lieu->setCarte($nouveauCheminRelatif);
+                $prefix = 'lieu-' . $lieu->getNom() . '-carte';
+                $lieu->setCarte($fileHandler->handle($nouvelleCarte, null, $prefix, 'lieux'));
             } else { $lieu->setCarte('assets/img/placeholders/1280x720.png'); }
 
+            $nouvelleRegion = $form->get('region')->getData();
             if (!empty($nouvelleRegion)) {
-                $nouvelleRegionNomFichier = $uploadeur->upload($nouvelleRegion, 'lieu-' . $lieu->getNom() . '-region', 'lieux');
-                $nouveauCheminRelatif = 'assets/img/lieux/' . $nouvelleRegionNomFichier;
-                $lieu->setRegion($nouveauCheminRelatif);
+                $prefix = 'lieu-' . $lieu->getNom() . '-region';
+                $lieu->setRegion($fileHandler->handle($nouvelleRegion, null, $prefix, 'lieux'));
             } else { $lieu->setRegion('assets/img/placeholders/1280x720.png'); }
 
             $em->persist($lieu);
             $em->flush();
 
-            $this->addFlash('success', 'Le Lieu a bien été ajouté !');
+            $this->addFlash('success', 'Le Lieu a bien été ajouté.');
 
             // REDIRECTION
             if (!empty($request->query->get('redirect')) && $request->query->get('redirect') == 'lieu')
@@ -81,7 +77,7 @@ class AdminLieuController extends AbstractController
      * @Route("/admin/lieu/{id}/edit", name="admin_lieu_edit")
      * @IsGranted("ROLE_MJ")
      */
-    public function editerLieu(Request $request, Lieu $lieu, Uploader $uploadeur): Response {
+    public function editerLieu(Request $request, Lieu $lieu, FileHandler $fileHandler): Response {
 
         $form = $this->createForm(AdminLieuType::class, $lieu);
         
@@ -93,59 +89,27 @@ class AdminLieuController extends AbstractController
             $nouvelleCarte = $form->get('carte')->getData();
             $nouvelleRegion = $form->get('region')->getData();
 
+
+            $nouvelleImage = $form->get('image')->getData();
             if (!empty($nouvelleImage)) {
-                $ancienneImageNomFichier = basename($lieu->getImage());
-
-                $nouvelleImageNomFichier = $uploadeur->upload($nouvelleImage, 'lieu-' . $lieu->getNom() . '-image', 'lieux');
-                $nouveauCheminRelatif = 'assets/img/lieux/' . $nouvelleImageNomFichier;
-                $lieu->setImage($nouveauCheminRelatif);
-
-                // Efface l'ancien fichier uniquement s'il a réussit à récupérer depuis l'url un nom de fichier et que ce n'est pas un dossier (protection)
-                if ($ancienneImageNomFichier) {
-                    $ancienneImageCheminComplet = $this->getParameter('image_directory') . '/lieux/' . $ancienneImageNomFichier;
-                    if (is_dir($ancienneImageCheminComplet) == false) {
-                        $filesystem = new Filesystem();
-                        $filesystem->remove($ancienneImageCheminComplet);
-                    }
-                }
+                $prefix = 'lieu-' . $lieu->getNom() . '-image';
+                $lieu->setImage($fileHandler->handle($nouvelleImage, $lieu->getImage(), $prefix, 'lieux'));
             }
 
+            $nouvelleCarte = $form->get('carte')->getData();
             if (!empty($nouvelleCarte)) {
-                $ancienneCarteNomFichier = basename($lieu->getCarte());
-
-                $nouvelleCarteNomFichier = $uploadeur->upload($nouvelleCarte, 'lieu-' . $lieu->getNom() . '-carte', 'lieux');
-                $nouveauCheminRelatif = 'assets/img/lieux/' . $nouvelleCarteNomFichier;
-                $lieu->setCarte($nouveauCheminRelatif);
-
-                // Efface l'ancien fichier uniquement s'il a réussit à récupérer depuis l'url un nom de fichier et que ce n'est pas un dossier (protection)
-                if ($ancienneCarteNomFichier) {
-                    $ancienneCarteCheminComplet = $this->getParameter('image_directory') . '/lieux/' . $ancienneCarteNomFichier;
-                    if (is_dir($ancienneCarteCheminComplet) == false) {
-                        $filesystem = new Filesystem();
-                        $filesystem->remove($ancienneCarteCheminComplet);
-                    }
-                }
+                $prefix = 'lieu-' . $lieu->getNom() . '-carte';
+                $lieu->setCarte($fileHandler->handle($nouvelleCarte, $lieu->getCarte(), $prefix, 'lieux'));
             }
 
+            $nouvelleRegion = $form->get('region')->getData();
             if (!empty($nouvelleRegion)) {
-                $ancienneRegionNomFichier = basename($lieu->getRegion());
-
-                $nouvelleRegionNomFichier = $uploadeur->upload($nouvelleRegion, 'lieu-' . $lieu->getNom() . '-region', 'lieux');
-                $nouveauCheminRelatif = 'assets/img/lieux/' . $nouvelleRegionNomFichier;
-                $lieu->setRegion($nouveauCheminRelatif);
-
-                // Efface l'ancien fichier uniquement s'il a réussit à récupérer depuis l'url un nom de fichier et que ce n'est pas un dossier (protection)
-                if ($ancienneRegionNomFichier) {
-                    $ancienneRegionCheminComplet = $this->getParameter('image_directory') . '/lieux/' . $ancienneRegionNomFichier;
-                    if (is_dir($ancienneRegionCheminComplet) == false) {
-                        $filesystem = new Filesystem();
-                        $filesystem->remove($ancienneRegionCheminComplet);
-                    }
-                }
+                $prefix = 'lieu-' . $lieu->getNom() . '-region';
+                $lieu->setRegion($fileHandler->handle($nouvelleRegion, $lieu->getRegion(), $prefix, 'lieux'));
             }
 
             $this->getDoctrine()->getManager()->flush();
-            $this->addFlash('success', 'Le Lieu a bien été modifié !');
+            $this->addFlash('success', 'Le Lieu a bien été modifié.');
 
             // REDIRECTION
             if (!empty($request->query->get('redirect')) && $request->query->get('redirect') == 'lieu')
@@ -165,24 +129,20 @@ class AdminLieuController extends AbstractController
      * @Route("/admin/lieu/{id}/delete", name="admin_lieu_delete", methods={"GET"})
      * @IsGranted("ROLE_MJ")
      */
-    public function supprimerLieu(Request $request, Lieu $lieu): Response {
+    public function supprimerLieu(Request $request, Lieu $lieu, FileHandler $fileHandler): Response {
 
         if ($this->isCsrfTokenValid('delete' . $lieu->getId(), $request->query->get('csrf'))) {
 
             $entityManager = $this->getDoctrine()->getManager();
 
-            $nomImageASupprimer = basename($lieu->getImage());
-            $cheminImageASupprimer = $this->getParameter('image_directory') . '/lieux/' . $nomImageASupprimer;
-
-            if (file_exists($cheminImageASupprimer) && is_dir($cheminImageASupprimer) == false) {
-                $filesystem = new Filesystem();
-                $filesystem->remove($cheminImageASupprimer);
-            }
+            $fileHandler->handle(null, $lieu->getImage(), null, 'lieux');
+            $fileHandler->handle(null, $lieu->getCarte(), null, 'lieux');
+            $fileHandler->handle(null, $lieu->getRegion(), null, 'lieux');
 
             $entityManager->remove($lieu);
             $entityManager->flush();
 
-            $this->addFlash('success', 'Le Lieu a bien été supprimé !');
+            $this->addFlash('success', 'Le Lieu a bien été supprimé.');
         }
 
         return $this->redirectToRoute('admin_lieu');

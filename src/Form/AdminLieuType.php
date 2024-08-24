0b07ffe6233eeac4836077a829a106a85ec21815
Axel <axel.ein@gmail.com>
2024-08-24 18:43:55 +0200

    [LOCATION formType] added type and population property

--- src/Form/AdminLieuType.php
+++ src/Form/AdminLieuType.php
@@ -12,6 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class AdminLieuType extends AbstractType
 {
@@ -22,11 +23,40 @@ class AdminLieuType extends AbstractType
             ->add('surnom', TextType::class)
             ->add('icone', FileType::class, array('mapped' => false, 'data_class' => null, 'required' => false))
             ->add('image', FileType::class, array('mapped' => false, 'data_class' => null, 'required' => false))
+            ->add('type', ChoiceType::class, [
+                'placeholder' => 'Pas de type défini',
+                'required' => false,
+                'choices'  => [
+                    'Village' => 'Village',
+                    "Ville" => "Ville",
+                    'Cité' => 'Cité',
+                    'Région ou Royaume' => 'Région ou Royaume',
+                    'Shiro (Fort ou Château)' => 'Shiro (Fort ou Château)',
+                    'Kyuden (Palais)' => 'Kyuden (Palais)',
+                ],
+            ])
+            ->add('region', FileType::class, array(
+                'mapped' => false,
+                'data_class' => null,
+                'required' => false,
+                'label' => 'Carte de la région',
+            ))
+            ->add('carte', FileType::class, array(
+                'mapped' => false,
+                'data_class' => null,
+                'required' => false,
+                'label' => 'Plan Intérieur',
+            ))
             ->add('description', TextareaType::class, array('required' => false))
             ->add('quartiers', TextareaType::class, array('required' => false))
             ->add('locX', NumberType::class, array(
-                'scale' => 3,
-                'label' => 'Coordinate (Horizontal % of the map)',
+                'scale' => 2,
+                'attr' => array(
+                    'min' => 1,
+                    'max' => 99,
+                    'step' => '.05',
+                ),
+                'label' => 'Coord. % hori. carte',
                 'required' => false,
             ))
             ->add('locY', NumberType::class, array(
@@ -36,7 +66,11 @@ class AdminLieuType extends AbstractType
                     'max' => 99,
                     'step' => '.05',
                 ),
-                'label' => 'Coordinate (Vertical % of the map)',
+                'label' => 'Coord. % vert. carte',
+                'required' => false,
+            ))
+            ->add('population', NumberType::class, array(
+                'label' => 'Population',
                 'required' => false,
             ))
             ->add('clan', EntityType::class, [

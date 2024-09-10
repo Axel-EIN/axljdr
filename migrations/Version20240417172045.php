091737e96300383136adc384f808978add4992eb
Axel <axel.ein@gmail.com>
2024-09-10 22:49:18 +0200

    [PERSONNAGE migrations] added

---
+++ migrations/Version20240417172045.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240417172045 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE personnage CHANGE icone icone VARCHAR(255) DEFAULT NULL, CHANGE illustration illustration VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE utilisateur CHANGE roles roles JSON NOT NULL COMMENT \'(DC2Type:json)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE personnage CHANGE icone icone VARCHAR(255) NOT NULL, CHANGE illustration illustration VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE utilisateur CHANGE roles roles JSON NOT NULL COMMENT \'(DC2Type:json)\'');
+    }
+}

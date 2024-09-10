635434c5e2d3c29729104d4f08b20b223e0afc1b
Axel <axel.ein@gmail.com>
2024-09-10 23:05:50 +0200

    [SCRIPTS scriptJS] edited

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,19 +1,38 @@
-// Gestion du bouton Voir les Episodes qui se Replier
+// Page Aventure : Gestion du bouton Voir les Episodes
 $('.cacher, .voir').click(function(){
 
-    $(this).text(function(i,old){
+        if ($(this).hasClass('voir')) {
+            $(this).removeClass('voir').addClass('cacher');
+            $(this).removeClass('btn-primary-style').addClass('btn-tertiary-style').html('<i class="fa-solid fa-eye-slash" alt="Icône cacher" aria-hidden="true"></i>&nbsp;&nbsp;Cacher');
+        } else {
+            $(this).removeClass('cacher').addClass('voir');
+            $(this).removeClass('btn-tertiary-style').addClass('btn-primary-style').html('<i class="fa-solid fa-scroll fa-bounce" alt="Icône Voir" aria-hidden="true"></i>&nbsp;&nbsp;Voir les Sessions');
+        }
 
-        if ($(this).hasClass('voir'))
-        {
-            $(this).toggleClass('cacher').toggleClass('voir');
-            return 'Replier';
-        }
-        else if ($(this).hasClass('cacher'))
-        {
-            $(this).toggleClass('voir').toggleClass('cacher'); 
-            return 'Voir les Episodes';
-        }
-        
-    });
+});
+
+// Page Aventure : Activation/Desactivation de l'animation bounce sur le bouton lire des cartes episodes lors d'un mouseHover / mouseLeave
+$('.session-card-spacing').hover(mouseEnter, mouseLeave);
+
+function mouseEnter() {
+    $(this).find('.btn-primary-style').addClass('fa-bounce');
+};
+
+function mouseLeave() {
+    $(this).find('.btn-primary-style').removeClass('fa-bounce');
+};
+
+// Capture
+$('#caption').click(function (e) {
+
+    const x = e.offsetX,
+    y = e.offsetY;
+
+    // alert(x + ',' + y);
+
+    const xPourcent = x / 280 * 100;
+    const yPourcent = y / 400 * 100;
+
+    alert(xPourcent + '% ,' + yPourcent + '%');
 
 });

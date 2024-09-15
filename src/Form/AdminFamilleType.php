608f19c2695f16f2036a1de112ec63a279968988
Axel <axel.ein@gmail.com>
2024-09-15 17:52:12 +0200

    [GM-PANEL family-formtype] added

---
+++ src/Form/AdminFamilleType.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Famille;
+use App\Entity\Clan;
+use App\Entity\Personnage;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\Length;
+
+class AdminFamilleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, array( 'constraints' => [ new Length( [ 'max' => 30 ] ) ] ) )
+            ->add('mon', FileType::class, [
+                'mapped' => false, 'data_class' => null, 'required' => false,
+                'constraints' => [new File(['maxSize' => '5M'])],
+            ])
+            ->add('description', TextareaType::class, array( 'required' => false, 'constraints' => [ new Length( [ 'max' => 1000 ] ) ] ))
+            ->add('bonusStatNom', ChoiceType::class, [
+                'label' => 'Trait bonus (+1)',
+                'placeholder' => 'Aucun bonus',
+                'required' => false,
+                'choices' => [
+                    'Constitution'  => 'constitution',
+                    'Volonté'       => 'volonte',
+                    'Réflexes'      => 'reflexes',
+                    'Intuition'     => 'intuition',
+                    'Agilité'       => 'agilite',
+                    'Intelligence'  => 'intelligence',
+                    'Force'         => 'forceStat',
+                    'Perception'    => 'perception',
+                    'Vide'          => 'vide',
+                ],
+            ])
+            ->add('clan', EntityType::class, [
+                'class' => Clan::class,
+                'choice_label' => 'nom',
+                'placeholder' => 'Non défini',
+                'required' => false
+                ])
+            ->add('chef', EntityType::class, [
+                'class' => Personnage::class,
+                'choice_label' => 'prenom',
+                'group_by' => 'clan.nom',
+                'placeholder' => 'Non défini',
+                'required' => false
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Famille::class,
+        ]);
+    }
+}

c169d7fb34eab324f12bc9cbe0a62626b79a9aba
Axel <axel.ein@gmail.com>
2024-10-20 17:50:35 +0200

    [SCHOOL controller] fixed redirection not working

--- src/Controller/AdminEcoleController.php
+++ src/Controller/AdminEcoleController.php
@@ -70,8 +70,8 @@ class AdminEcoleController extends AbstractController
             $this->addFlash('success', 'L\'école a bien été ajoutée.');
 
             // REDIRECTION
-            if (!empty($request->query->get('redirect')) && $request->query->get('redirect') == 'regles')
-                return $this->redirectToRoute('regles');
+            if (!empty($request->query->get('redirect')) && $request->query->get('redirect') == 'ecole')
+                return $this->redirectToRoute('regles_ecole', ['id' => $ecole->getId()]);
             return $this->redirectToRoute('admin_ecole');
             
         } else {

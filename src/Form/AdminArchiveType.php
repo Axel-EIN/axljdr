6dc7ce6ed649357895217a991a67d277b0facd27
axel-EIN <axel.ein@gmail.com>
2026-03-11 17:00:25 +0100

    [ARCHIVE formType] added max file size constraint

--- src/Form/AdminArchiveType.php
+++ src/Form/AdminArchiveType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
@@ -19,7 +20,10 @@ class AdminArchiveType extends AbstractType
     {
         $builder
             ->add('titre', TextType::class)
-            ->add('image', FileType::class, array('mapped' => false, 'data_class' => null, 'required' => false))
+            ->add('image', FileType::class, [
+                'mapped' => false, 'data_class' => null, 'required' => false,
+                'constraints' => [new File(['maxSize' => '5M'])],
+            ])
             ->add('contenu', TextareaType::class)
             ->add('auteur', EntityType::class, [
                 'class' => Personnage::class,

8e1cfbdff11a698fd067fdb13643cdd22e0be776
Axel <axel.ein@gmail.com>
2026-05-04 03:34:16 +0200

    [SKILL migrations] added

---
+++ migrations/Version20231110185227.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231110185227 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE competence ADD degradante TINYINT(1) DEFAULT NULL, ADD globale TINYINT(1) DEFAULT NULL, ADD mot_cle1 VARCHAR(60) DEFAULT NULL, ADD mot_cle2 VARCHAR(60) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE competence DROP degradante, DROP globale, DROP mot_cle1, DROP mot_cle2');
+    }
+}

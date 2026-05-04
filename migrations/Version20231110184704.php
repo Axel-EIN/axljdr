b772c098e000ff4966d3b14144a75b21812ad669
Axel <axel.ein@gmail.com>
2026-05-04 03:34:16 +0200

    [SKILL migrations] added

---
+++ migrations/Version20231110184704.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231110184704 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE competence DROP sous_type, DROP sous_type2, CHANGE type categorie VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE competence ADD sous_type VARCHAR(60) DEFAULT NULL, ADD sous_type2 VARCHAR(60) DEFAULT NULL, CHANGE categorie type VARCHAR(255) NOT NULL');
+    }
+}

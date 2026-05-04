c461ddfdc38c3ca1e70d6e3ac1e2d7caf96ab3d7
Axel <axel.ein@gmail.com>
2026-05-04 13:48:04 +0200

    [SPELL migrations] added

---
+++ migrations/Version20231129093841.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231129093841 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE sort CHANGE keword2 keyword2 VARCHAR(60) DEFAULT NULL, CHANGE keword3 keyword3 VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE sort CHANGE keyword3 keword3 VARCHAR(255) DEFAULT NULL, CHANGE keyword2 keword2 VARCHAR(60) DEFAULT NULL');
+    }
+}

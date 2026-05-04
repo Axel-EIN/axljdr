822df9fe29b4ee6a5e3c8cc053bfd24a5b36a91f
Axel <axel.ein@gmail.com>
2026-05-04 03:45:10 +0200

    [LIBRARY migrations] added

---
+++ migrations/Version20231129091325.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231129091325 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE library ADD keyword1_field VARCHAR(255) DEFAULT NULL, ADD keyword2_field VARCHAR(255) DEFAULT NULL, ADD keyword3_field VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE library DROP keyword1_field, DROP keyword2_field, DROP keyword3_field');
+    }
+}

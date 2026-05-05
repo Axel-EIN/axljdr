cc7722c14a55cb1f0ba5531015fc2a55248142c9
Axel <axel.ein@gmail.com>
2026-05-05 04:55:56 +0200

    [CLAN migrations] added

---
+++ migrations/Version20260826120001.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20260826120001 extends AbstractMigration
+{
+  public function getDescription(): string
+  {
+    return 'Ajoute la colonne locked à clan (verrou MJ, identique à archive, lieu et personnage).';
+  }
+
+  public function up(Schema $schema): void
+  {
+    $this->addSql('ALTER TABLE clan ADD locked TINYINT(1) NOT NULL');
+  }
+
+  public function down(Schema $schema): void
+  {
+    $this->addSql('ALTER TABLE clan DROP locked');
+  }
+}

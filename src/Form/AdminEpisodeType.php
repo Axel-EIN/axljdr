887ee2e78d598a3f6e24ce2f7c2c33c6aaae2153
Axel <axel.ein@gmail.com>
2026-05-05 03:59:53 +0200

    [EPISODE formType] added

---
+++ src/Form/AdminEpisodeType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Episode;
+use App\Entity\Chapitre;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class AdminEpisodeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('numero', IntegerType::class)
+            ->add('numeroSaison', IntegerType::class)
+            ->add('titre', TextType::class)
+            ->add('resume', TextareaType::class, ['required' => false, 'constraints' => [ new Length( [ 'max' => 200 ] ) ]])
+            ->add('image', FileType::class, [
+                'mapped' => false, 'data_class' => null, 'required' => false,
+                'constraints' => [new File(['maxSize' => '5M'])],
+            ])
+            ->add('chapitreParent', EntityType::class, [
+                'class' => Chapitre::class,
+                'choice_label' => 'titre'
+            ])
+            ->add('issue', ChoiceType::class, [
+                'choices'  => [
+                    '' => null,
+                    'Succès' => 'Win',
+                    'Succès mitigé' => 'Mitigate',
+                    "Échec" => "Lose"]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Episode::class,
+        ]);
+    }
+}

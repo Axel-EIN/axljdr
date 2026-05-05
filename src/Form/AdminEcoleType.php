bb3adb417896fa5b73bac19febc53c34aa2c1480
Axel <axel.ein@gmail.com>
2026-05-05 02:53:46 +0200

    [ECOLE formType] added

---
+++ src/Form/AdminEcoleType.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Clan;
+use App\Entity\Ecole;
+use App\Entity\Classe;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\Length;
+
+class AdminEcoleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [ 'constraints' => [ new Length( [ 'max' => 60 ] ) ] ] )
+            ->add('classe', EntityType::class, [
+                'class' => Classe::class,
+                'choice_label' => 'nom'
+            ])
+            ->add('clan', EntityType::class, [
+                'class' => Clan::class,
+                'choice_label' => 'nom'
+            ])
+
+            // Nullables Fields
+            ->add('image', FileType::class, [
+                'required' => false, 'mapped' => false, 'data_class' => null,
+                'label' => 'Image (facultative)',
+                'constraints' => [new File(['maxSize' => '5M'])],
+            ])
+            ->add('description', TextareaType::class, [ 'required' => false, 'constraints' => [ new Length( [ 'max' => 2000 ] ) ] ])
+            ->add('bonus', TextType::class, [ 'required' => false, 'constraints' => [ new Length( [ 'max' => 20 ] ) ] ])
+            ->add('competences', TextareaType::class, [ 'required' => false, 'constraints' => [ new Length( [ 'max' => 600 ] ) ] ])
+            ->add('equipements', TextareaType::class, [ 'required' => false, 'constraints' => [ new Length( [ 'max' => 600 ] ) ] ])
+            ->add('tech1Nom', TextType::class, ['required' => false, 'constraints' => [ new Length( [ 'max' => 100 ] ) ] ])
+            ->add('tech1Desc', TextareaType::class, ['required' => false] )
+            ->add('tech2Nom', TextType::class, ['required' => false, 'constraints' => [ new Length( [ 'max' => 100 ] ) ]])
+            ->add('tech2Desc', TextareaType::class, ['required' => false] )
+            ->add('tech3Nom', TextType::class, ['required' => false, 'constraints' => [ new Length( [ 'max' => 100 ] ) ] ])
+            ->add('tech3Desc', TextareaType::class, ['required' => false ] )
+            ->add('tech4Nom', TextType::class, ['required' => false, 'constraints' => [ new Length( [ 'max' => 100 ] ) ] ])
+            ->add('tech4Desc', TextareaType::class, ['required' => false ] )
+            ->add('tech5Nom', TextType::class, ['required' => false, 'constraints' => [ new Length( [ 'max' => 100 ] ) ] ])
+            ->add('tech5Desc', TextareaType::class, ['required' => false])
+            ->add('techSpecialNom', TextType::class, ['required' => false, 'constraints' => [ new Length( [ 'max' => 100 ] ) ] ])
+            ->add('techSpecialDesc', TextareaType::class, ['required' => false])
+            ->add('affinite', TextType::class, ['required' => false, 'constraints' => [ new Length( [ 'max' => 50 ] ) ] ])
+            ->add('deficience', TextType::class, ['required' => false, 'constraints' => [ new Length( [ 'max' => 50 ] ) ] ])
+            ->add('sorts', TextareaType::class, ['required' => false])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Ecole::class,
+        ]);
+    }
+}

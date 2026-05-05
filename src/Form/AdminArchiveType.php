5a1badf5fc890f2b0324034fe86aa76b8d1fb3c1
Axel <axel.ein@gmail.com>
2026-05-05 02:49:58 +0200

    [GM-PANEL archive-formtype] added

---
+++ src/Form/AdminArchiveType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Archive;
+use App\Entity\Personnage;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
+class AdminArchiveType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre', TextType::class)
+            ->add('image', FileType::class, [
+                'mapped' => false, 'data_class' => null, 'required' => false,
+                'constraints' => [new File(['maxSize' => '5M'])],
+            ])
+            ->add('contenu', TextareaType::class)
+            ->add('auteur', EntityType::class, [
+                'class' => Personnage::class,
+                'choice_label' => 'prenom',
+                'placeholder' => 'Non défini',
+                'required' => false
+            ])
+            ->add('locked', CheckboxType::class, ['required' => false])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Archive::class,
+        ]);
+    }
+}

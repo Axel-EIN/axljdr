c5806847f9b115ed1b4762d8623f0757127da799
axel-EIN <axel.ein@gmail.com>
2026-05-05 12:28:29 +0200

    [ACCOUNT controller] added

---
+++ src/Controller/MonCompteController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\FileHandler;
+use App\Form\MonCompteMdpType;
+use App\Form\MonCompteAvatarType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+/**
+ * @IsGranted("ROLE_USER")
+ */
+class MonCompteController extends AbstractController
+{
+    /**
+     * @Route("/mon_compte", name="mon_compte")
+     */
+    public function afficherMonCompte(): Response
+    {
+        return $this->render('mon_compte/index.html.twig');
+    }
+
+    /**
+     * @Route("/mon_compte/password/edit", name="mon_compte_mdp")
+     */
+    public function modifierMotDePasse(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(MonCompteMdpType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasherInterface->hashPassword(
+                        $user,
+                        $form->get('plainPassword')->getData()
+                    )
+                );
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('success', 'Votre mot de passe a bien été modifié !');
+            return $this->redirectToRoute('mon_compte');
+        }
+
+        return $this->render('mon_compte/modifier_mdp.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/mon_compte/avatar/edit", name="mon_compte_avatar")
+     */
+    public function modifierAvatar(Request $request, FileHandler $fileHandler): Response
+    {
+        $utilisateur = $this->getUser();
+        $form = $this->createForm(MonCompteAvatarType::class, $utilisateur);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $nouveauAvatar = $form->get('avatar')->getData();
+
+            // Passe par FileHandler pour le ré-encodage GD : c'est le seul upload
+            // ouvert aux joueurs, donc le seul où les octets sont hostiles.
+            if (!empty($nouveauAvatar)) {
+                $utilisateur->setAvatar($fileHandler->handle(
+                    $nouveauAvatar,
+                    $utilisateur->getAvatar(),
+                    'avatar-' . $utilisateur->getPseudo(),
+                    'avatars',
+                    'square320'
+                ));
+            }
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($utilisateur);
+            $em->flush();
+
+            $this->addFlash('success', 'Votre avatar a bien été modifié !');
+            return $this->redirectToRoute('mon_compte');
+        }
+
+        return $this->render('mon_compte/modifier_avatar.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+}

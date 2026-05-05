dd9c0920d6dccb3765e4d770c6abf2c82a1e21a8
Axel <axel.ein@gmail.com>
2026-05-05 04:05:12 +0200

    [SCENE formType] added

---
+++ src/Form/AdminSceneType.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Scene;
+use App\Entity\Episode;
+use App\Entity\Lieu;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Validator\Constraints\File;
+
+class AdminSceneType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre', TextType::class)
+            ->add('episodeParent', EntityType::class, [
+                'class' => Episode::class,
+                'choice_label' => 'titre'
+            ])
+            ->add('numero', IntegerType::class)
+            ->add('lieu', EntityType::class, [
+                'placeholder' => 'Pas de lieu défini',
+                'required' => false,
+                'class' => Lieu::class,
+                'choice_label' => 'nom'
+            ])
+            ->add('temps', ChoiceType::class, [
+                'placeholder' => 'Pas de temps défini',
+                'required' => false,
+                'choices'  => [
+                    'Aube' => 'Aube',
+                    'Matinée' => 'Matinée',
+                    "Midi" => "Midi",
+                    'Après-midi' => 'Après-midi',
+                    'Journée' => 'Journée',
+                    'Soirée' => 'Soirée',
+                    'Crépuscule' => 'Crépuscule',
+                    'Minuit' => 'Minuit',
+                    'Nuit' => 'Nuit',
+                    'Jours suivants' => 'Jours suivants',
+                    'Intemporel' => 'Intemporel',
+                ], ])
+            ->add('texte', TextareaType::class)
+            ->add('image', FileType::class, [
+                'label' => "Image ( idéal 1280x720 )",
+                'mapped' => false,
+                'data_class' => null,
+                'required' => false,
+                'constraints' => [new File(['maxSize' => '5M'])],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Scene::class,
+        ]);
+    }
+}

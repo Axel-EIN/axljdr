96b6e786716049050ac717e55a8af6ea9617dd53
Axel <axel.ein@gmail.com>
2026-05-05 03:40:00 +0200

    [SEASON repository] added

---
+++ src/Repository/SaisonRepository.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Saison;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Saison|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Saison|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Saison[]    findAll()
+ * @method Saison[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class SaisonRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Saison::class);
+    }
+
+    public function findPrevious($numero) {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.numero = :value_numero')
+            ->setParameter('value_numero', $numero-1)
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+
+    public function findNext($numero) {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.numero = :value_numero')
+            ->setParameter('value_numero', $numero+1)
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+
+    /** La saison en cours, c'est-a-dire la derniere creee. */
+    public function findCourante(): ?Saison {
+        return $this->findOneBy([], ['numero' => 'DESC']);
+    }
+
+    public function countSaisons() {
+        return $this->createQueryBuilder('s')
+            ->select('count(s.id)')
+            ->getQuery()
+            ->getSingleScalarResult();
+    }
+}

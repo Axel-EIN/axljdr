278a89f1548efc855cf6a28cc66ec36d9d73d515
Axel-EIN <axel.ein@gmail.com>
2026-09-07 18:46:01 +0200

    [SKILL Entity] added publishable trait

--- src/Entity/Competence.php
+++ src/Entity/Competence.php
@@ -7,9 +7,12 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=CompetenceRepository::class)
+ * @ORM\HasLifecycleCallbacks
  */
 class Competence
 {
+    use PublishableTrait;
+
     /**
      * @ORM\Id
      * @ORM\GeneratedValue

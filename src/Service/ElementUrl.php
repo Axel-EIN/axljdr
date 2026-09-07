d6344879279dd279f7c61cd1a282aa6b1f24b101
Axel-EIN <axel.ein@gmail.com>
2026-09-07 19:07:43 +0200

    [SERVICE element-url] added

---
+++ src/Service/ElementUrl.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Service;
+
+use App\Repository\LibraryRepository;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class ElementUrl
+{
+    private $router;
+    private $libraryRepository;
+    private $libraries;
+
+    public function __construct(UrlGeneratorInterface $router, LibraryRepository $libraryRepository)
+    {
+        $this->router = $router;
+        $this->libraryRepository = $libraryRepository;
+    }
+
+    public function of($element): ?string
+    {
+        $key = EntityRegistry::keyOf($element);
+
+        if ($key === null) {
+            return null;
+        }
+
+        $route = EntityRegistry::route($key);
+
+        if ($route !== null) {
+            return $this->router->generate($route, ['id' => $element->getId()]);
+        }
+
+        $library = $this->libraries()[$key] ?? null;
+
+        return $library === null ? null : $this->router->generate('regles_library', ['id' => $library]);
+    }
+
+    private function libraries(): array
+    {
+        if ($this->libraries !== null) {
+            return $this->libraries;
+        }
+
+        $this->libraries = [];
+        foreach ($this->libraryRepository->findBy([], ['base' => 'DESC', 'numero' => 'ASC']) as $library) {
+            $key = $library->getEntity();
+            if (!empty($key) && !isset($this->libraries[$key])) {
+                $this->libraries[$key] = $library->getId();
+            }
+        }
+
+        return $this->libraries;
+    }
+}
